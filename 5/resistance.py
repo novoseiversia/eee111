f040f2da3147424730d01ee127e37c2815417f18
Nile Jocson <novoseiversia@gmail.com>
2024-09-30 12:17:08 +0800

    Add week 5 resistance.py

---
+++ 5/resistance.py
@@ -0,0 +1,84 @@
+# SPDX-FileCopyrightText: Copyright (C) Nile Jocson <novoseiversia@gmail.com>
+# SPDX-License-Identifier: 0BSD
+
+
+
+from typing import List
+
+
+
+valid_colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "gray", "white", "gold", "silver"]
+
+def color(s: str) -> str:
+	lower_s = s.lower()
+	if lower_s in valid_colors:
+		return lower_s
+	else:
+		raise ValueError(f"Invalid color: { lower_s }")
+
+
+
+def input_list_colors(prompt: str) -> List[str]:
+	while True:
+		try:
+			parsed = [color(s) for s in input(prompt).split()]
+		except ValueError:
+			print("Please enter a valid list of colors.")
+		else:
+			return parsed
+
+
+
+def get_band_digit(color: str) -> int:
+	digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, None, None]
+	band_digits = dict(zip(valid_colors, digits, strict = True))
+	return band_digits[color]
+
+def get_band_multiplier(color: str) -> float:
+	exponents = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -2]
+	band_exponents = dict(zip(valid_colors, exponents, strict = True))
+	return 10**band_exponents[color]
+
+def get_band_tolerance(color: str) -> float:
+	tolerances = [None, 1, 2, 3, 4, 0.5, 0.25, 0.10, 0.05, None, 5, 10]
+	band_tolerances = dict(zip(valid_colors, tolerances, strict = True))
+	return band_tolerances[color]
+
+
+
+def __main__():
+	colors = input_list_colors("Enter your 4-band resistor colors: ")
+
+	if len(colors) != 4:
+		print("Please enter a color combination for a 4-band resistor.")
+		return
+
+	first_digit  = get_band_digit(colors[0])
+	second_digit = get_band_digit(colors[1])
+	multiplier   = get_band_multiplier(colors[2])
+	tolerance    = get_band_tolerance(colors[3])
+
+	error = False
+
+	if first_digit == None:
+		print(f"Invalid first digit color \"{ colors[0] }\"")
+		error = True
+	if second_digit == None:
+		print(f"Invalid second digit color \"{ colors[1] }\"")
+		error = True
+	if multiplier == None:
+		print(f"Invalid multiplier color \"{ colors[2] }\"")
+		error = True
+	if tolerance == None:
+		print(f"Invalid tolerance color \"{ colors[3] }\"")
+		error = True
+	if error:
+		return
+
+	resistance = (10 * first_digit + second_digit) * multiplier
+
+	print(f"{ resistance } +/- { tolerance }%")
+
+
+
+__main__()
