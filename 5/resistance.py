54bb7f487ef72cf969d4af1620302653c43dd3da
Nile Jocson <novoseiversia@gmail.com>
2024-10-01 11:31:26 +0800

    Add documentation for resistance.py

--- 5/resistance.py
+++ 5/resistance.py
@@ -10,6 +10,26 @@ from typing import List
 valid_colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white", "gold", "silver"]
 
 def color(s: str) -> str:
+	"""
+	Checks if the string is a valid color (case-insensitive).
+
+	Parameters
+	----------
+	s : str
+		The string to validate
+
+	Returns
+	-------
+	str
+		A lowercased copy of s
+		Must be one of: black, brown, red, orange, yellow, green, blue, violet, grey, white, gold, silver
+
+	Raises
+	------
+	ValueError
+		If str is not one of the valid colors listed above (case-insensitive)
+	"""
+
 	lower_s = s.lower()
 	if lower_s in valid_colors:
 		return lower_s
@@ -19,6 +39,21 @@ def color(s: str) -> str:
 
 
 def input_list_colors(prompt: str) -> List[str]:
+	"""
+	Asks the user for a space separated list of colors. Repeats until a valid input is received.
+
+	Parameters
+	----------
+	prompt : str
+		The prompt to display for input
+
+	Returns
+	-------
+	List[str]
+		A list of strings of colors.
+		A color must be one of: black, brown, red, orange, yellow, green, blue, violet, grey, white, gold, silver
+	"""
+
 	while True:
 		try:
 			parsed = [color(s) for s in input(prompt).split()]
@@ -30,21 +65,86 @@ def input_list_colors(prompt: str) -> List[str]:
 
 
 def get_band_digit(color: str) -> int | None:
+	"""
+	Returns a digit corresponding to the given resistor band color.
+
+	Parameters
+	----------
+	color : str
+		The band color
+		Must be one of: black, brown, red, orange, yellow, green, blue, violet, grey, white, gold, silver
+
+	Returns
+	-------
+	int
+		The digit corresponding to the band color, if a valid digit band color is given.
+	None
+		If the band color given is either gold or silver
+	"""
+
 	digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, None, None]
 	band_digits = dict(zip(valid_colors, digits, strict = True))
 	return band_digits[color]
 
 def get_band_multiplier(color: str) -> float | None:
+	"""
+	Returns a mnultiplier corresponding to the given resistor band color.
+
+	Parameters
+	----------
+	color : str
+		The band color
+		Must be one of: black, brown, red, orange, yellow, green, blue, violet, grey, white, gold, silver
+
+	Returns
+	-------
+	int
+		The multiplier corresponding to the band color.
+	"""
+
 	exponents = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -2]
 	band_exponents = dict(zip(valid_colors, exponents, strict = True))
 	return 10**band_exponents[color]
 
 def get_band_tolerance(color: str) -> float | None:
+	"""
+	Returns a tolerance corresponding to the given resistor band color.
+
+	Parameters
+	----------
+	color : str
+		The band color
+		Must be one of: black, brown, red, orange, yellow, green, blue, violet, grey, white, gold, silver
+
+	Returns
+	-------
+	int
+		The tolerance corresponding to the band color, if a valid tolerance band color is given.
+	None
+		If the band color given is either black or white
+	"""
+
 	tolerances = [None, 1, 2, 3, 4, 0.5, 0.25, 0.10, 0.05, None, 5, 10]
 	band_tolerances = dict(zip(valid_colors, tolerances, strict = True))
 	return band_tolerances[color]
 
 def get_resistance_from_colors(colors: List[str]) -> str:
+	"""
+	Calculates the resistance of a given 4-band resistor by its color bands.
+
+	Parameters
+	----------
+	colors: List[str]
+		The color bands.
+		A color must be one of: black, brown, red, orange, yellow, green, blue, violet, grey, white, gold, silver
+
+	Returns
+	-------
+	str
+		Returns the resistance of the resistor in form: { resistance } +/- { tolerance }%
+		If any color band is invalid, returns an error string.
+	"""
+
 	first_digit  = get_band_digit(colors[0])
 	second_digit = get_band_digit(colors[1])
 	multiplier   = get_band_multiplier(colors[2])
