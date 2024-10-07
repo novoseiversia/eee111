fd1cc4ef8c33b75aab951698b899be749ebb7733
Nile Jocson <novoseiversia@gmail.com>
2024-10-07 11:55:41 +0800

    Rewrite parse_args using parse_rules

--- sp1/jocson_nile_202400045_code.py
+++ sp1/jocson_nile_202400045_code.py
@@ -57,18 +57,43 @@ def parse_rules(
 
 
 def parse_args(command: List[str]) -> List[Any]:
-	if command[1] == "needed_now":
-		return CommandType.NEEDED_NOW.make(command[0])
-	elif command[1] == "needed_in":
-		return CommandType.NEEDED_IN.make(command[0], command[2])
-	elif command[1] == "runs_out":
-		return CommandType.RUNS_OUT.make(command[0])
-	elif command[2] == "run_outs":
-		return CommandType.RUN_OUTS.make(command[0], command[1])
-	elif command[0] == "exit":
-		return CommandType.EXIT.make()
+	if parsed := parse_rules(
+		[str, "needed_now"],
+		[(1, CommandType), 0],
+		command
+	):
+		return parsed
+
+	elif parsed := parse_rules(
+		[str, "needed_in", int],
+		[(1, CommandType), 0, 2],
+		command
+	):
+		return parsed
+
+	elif parsed := parse_rules(
+		[str, "runs_out"],
+		[(1, CommandType), 0],
+		command
+	):
+		return parsed
+
+	elif parsed := parse_rules(
+		[str, int, "runouts"],
+		[(2, CommandType), 0, 1],
+		command
+	):
+		return parsed
+
+	elif parsed := parse_rules(
+		["exit"],
+		[(0, CommandType)],
+		command
+	):
+		return parsed
+
 	else:
-		return CommandType.INVALID.make()
+		return [CommandType.INVALID]
 
 
 
