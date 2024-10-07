ce07184596c7035c5e8a3f5eac6ffda66f95bf19
Nile Jocson <novoseiversia@gmail.com>
2024-10-07 10:20:38 +0800

    Add CommandType enum

--- sp1/jocson_nile_202400045_code.py
+++ sp1/jocson_nile_202400045_code.py
@@ -1,7 +1,18 @@
 # SPDX-FileCopyrightText: Copyright (C) Nile Jocson <novoseiversia@gmail.com>
 # SPDX-License-Identifier: 0BSD
 
+from enum import Enum
 from typing import List
+
+
+
+class CommandType(Enum):
+	NEEDED_IN  =  0,
+	NEEDED_NOW =  1,
+	RUNS_OUT   =  2,
+	RUN_OUTS   =  3,
+	EXIT       =  4,
+	INVALID    = -1,
 
 
 
