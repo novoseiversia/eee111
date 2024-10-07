d5777e5a95c2c60e22595422f0adfa4c840be7da
Nile Jocson <novoseiversia@gmail.com>
2024-10-07 10:50:59 +0800

    Rewrite print test

--- sp1/jocson_nile_202400045_code.py
+++ sp1/jocson_nile_202400045_code.py
@@ -41,7 +41,7 @@ def parse_args(command: List[str]) -> Command:
 
 
 def __main__():
-	print(parse_args(input_list("Input your command:")))
+	print(parse_args(input_list("Input your command:"))[0].name())
 	return
 
 
