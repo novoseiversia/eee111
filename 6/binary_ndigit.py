913f79bd8914c7205989a8dc8f9e8397164e1235
Nile Jocson <novoseiversia@gmail.com>
2024-10-22 22:55:07 +0800

    Rename ndigit_binary to binary_ndigit

--- 6/binary_ndigit.py
+++ 6/binary_ndigit.py
@@ -14,29 +14,21 @@ def input_int(prompt: str) -> int:
 
 
 
-def ndigit_binary_z(n: int, z: int) -> list[str]:
+def binary_ndigit(n: int) -> list[str]:
 	if n == 1:
-		if z == 0:
-			return ["1"]
-		elif z == 1:
-			return ["0"]
-		else:
-			return []
+		return ["0", "1"]
 
 	out: list[str] = []
-	for b in ndigit_binary_z(n - 1, z):
-		out.append(b + "1")
-
-	for b in ndigit_binary_z(n - 1, z - 1):
+	for b in binary_ndigit(n - 1):
 		out.append(b + "0")
+		out.append(b + "1")
 
 	return out
 
 
 
 n = input_int("Input n: ")
-z = input_int("Input z: ")
 
-print(f"All { n }-digit binary numbers with { z } zero/es:")
-for b in ndigit_binary_z(n, z):
+print(f"All { n }-digit binary numbers:")
+for b in binary_ndigit(n):
 	print(b)
--- 6/binary_ndigit_even0_odd1.py
+++ 6/binary_ndigit_even0_odd1.py
@@ -14,7 +14,7 @@ def input_int(prompt: str) -> int:
 
 
 
-def ndigit_binary_z(n: int, z: int) -> list[str]:
+def binary_ndigit_z(n: int, z: int) -> list[str]:
 	if n == 1:
 		if z == 0:
 			return ["1"]
@@ -24,30 +24,30 @@ def ndigit_binary_z(n: int, z: int) -> list[str]:
 			return []
 
 	out: list[str] = []
-	for b in ndigit_binary_z(n - 1, z):
+	for b in binary_ndigit_z(n - 1, z):
 		out.append(b + "1")
 
-	for b in ndigit_binary_z(n - 1, z - 1):
+	for b in binary_ndigit_z(n - 1, z - 1):
 		out.append(b + "0")
 
 	return out
 
 
 
-def ndigit_binary_even0_odd1(n: int, z: int | None = None) -> list[str]:
+def binary_ndigit_even0_odd1(n: int, z: int | None = None) -> list[str]:
 	if n % 2 == 0:
 		return []
 	if z == None:
-		return ndigit_binary_even0_odd1(n, n - 1)
+		return binary_ndigit_even0_odd1(n, n - 1)
 	if z == 0:
-		return ndigit_binary_z(n, 0)
+		return binary_ndigit_z(n, 0)
 
-	return ndigit_binary_z(n, z) + ndigit_binary_even0_odd1(n, z - 2)
+	return binary_ndigit_z(n, z) + binary_ndigit_even0_odd1(n, z - 2)
 
 
 
 n = input_int("Input n: ")
 
 print(f"All { n }-digit binary numbers with an even amount of zeroes and an odd amount of ones:")
-for b in ndigit_binary_even0_odd1(n):
+for b in binary_ndigit_even0_odd1(n):
 	print(b)
